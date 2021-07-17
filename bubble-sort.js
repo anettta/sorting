aca7fad4b3fa4beaf32cee5714031607d52c0f65
Anya <annagapyuk@gmail.com>
2021-07-17 12:34:32 -0400

    initial commit - bubble sort

---
+++ bubble-sort.js
@@ -0,0 +1,16 @@
+function bubbleSort(array) {
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length; j++) {
+      if (array[j] > array[j + 1]) {
+        let temp = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = temp;
+      }
+    }
+  }
+}
+
+let nums = [1, 9, 0, 8, 3, 2, 7, 4, 5];
+bubbleSort(nums);
+
+console.log(nums);
