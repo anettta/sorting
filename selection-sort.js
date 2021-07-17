9310ac3a209fd014bc2f7401d6c93d1b1ca7e108
Anya <annagapyuk@gmail.com>
2021-07-17 13:03:21 -0400

    added selection sort

---
+++ selection-sort.js
@@ -0,0 +1,25 @@
+// Time Complexity O(n^2)
+// Space Complexity O(1)
+
+function selectionSort(array) {
+  for (let i = 0; i < array.length; i++) {
+    // set current index as minimum
+    let min = i;
+    let temp = array[i];
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[j] < array[min]) {
+        // update minimum if current is lower that what we had previously
+        min = j;
+      }
+    }
+    array[i] = array[min];
+
+    array[min] = temp;
+  }
+  return array;
+}
+
+let nums = [1, 9, 0, 8, 3, 2, 7, 4, 5];
+selectionSort(nums);
+
+console.log(nums);
